59093bd99e6d3bc7ffc4a9054a8b80fc4430dae6
Hylan Silva <hylansilva@outlook.com>
2024-10-06 18:20:46 -0300

    bkear

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-  // https://vitejs.dev/config/
-  export default defineConfig({
-    plugins: [react()],
-  })
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }, // Isso pode suprimir alguns erros comuns
+  },
+})
